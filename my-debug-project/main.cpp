2b7eaf1e2a9e138ec19dbdc02663f754f9bdb95d
Sami Meadad Choudhury <sami.meadad@gmail.com>
2025-05-19 21:26:18 +0600

    main file changed

--- my-debug-project/main.cpp
+++ my-debug-project/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
+    int sum = 0;
+    int a[] = {1,2,5,8,9};
+    for(auto x:a){
+        sum += x;
+    }
+    cout << "Sum =" <<sum <<endl;
     cout << "Hello world!" << endl;
     return 0;
 }
