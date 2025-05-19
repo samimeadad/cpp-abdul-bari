cd82edab05427d391da827581dbb58f8f8cccec6
Sami Meadad Choudhury <sami.meadad@gmail.com>
2025-05-20 00:07:05 +0600

    addition program added

---
+++ first-program.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    system("cls");
+    int a, b, sum;
+
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    sum = a+b;
+    cout<<"Sum: "<<sum <<endl;
+    //system("pause");
+    return 0;
+}
